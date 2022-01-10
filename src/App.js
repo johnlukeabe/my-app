f5d8c0e3e56df96c139a2ef8dd14c593ddf7b55f
Jungsu Sohn <jsson98@gmail.com>
2022-01-09 17:20:38 -0700

    React: Added ckEditor
    Signed-off-by: Jungsu Sohn <jsson98@gmail.com>

--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
 import React from 'react';
 import './App.css';
+import { CKEditor } from '@ckeditor/ckeditor5-react';
+import ClassicEditor from '@ckeditor/ckeditor5-build-classic';
 
 function App() {
   return (
@@ -12,8 +14,25 @@ function App() {
         </div>
       </div>
       <div className='form-wrapper'>
-        <input className='title-input' type='text' placeholder='Title' />
-        <textarea className='text-area' placeholder='text' />
+        <input className="title-input" type='text' placeholder='제목' />
+        <CKEditor
+          editor={ClassicEditor}
+          data=""
+          onReady={editor => {
+            // You can store the "editor" and use when it is needed.
+            console.log('Editor is ready to use!', editor);
+          }}
+          onChange={(event, editor) => {
+            const data = editor.getData();
+            console.log({ event, editor, data });
+          }}
+          onBlur={(event, editor) => {
+            console.log('Blur.', editor);
+          }}
+          onFocus={(event, editor) => {
+            console.log('Focus.', editor);
+          }}
+        />
       </div>
       <button className='submit-button'>Submit</button>
     </div>
